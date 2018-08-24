09e574f16467c967c45c8683913340ed7b56b43b
lucx1 <l.lucx2001@gmail.com>
2018-08-24 22:28:46 +0200

    Improved section and made navigation clickable
    Signed-off-by: lucx1 <l.lucx2001@gmail.com>

--- assets/js/grammar.js
+++ assets/js/grammar.js
@@ -1,7 +1,20 @@
-el1x0x0.style.display = "none";
-el2x0x0.style.display = "none";
-el3x0x0.style.display = "none";
-el4x0x0.style.display = "none";
+el1x1x0.style.display = "none";
+el2x1x0.style.display = "none";
+el2x1x0.style.display = "none";
+el2x2x0.style.display = "none";
+el2x3x0.style.display = "none";
+el2x4x0.style.display = "none";
+el2x5x0.style.display = "none";
+el2x6x0.style.display = "none";
+el2x7x0.style.display = "none";
+el3x1x0.style.display = "none";
+el3x2x0.style.display = "none";
+el3x3x0.style.display = "none";
+el3x4x0.style.display = "none";
+el3x5x0.style.display = "none";
+el4x1x0.style.display = "none";
+el4x2x0.style.display = "none";
+el4x3x0.style.display = "none";
 function expfun(x,y,z) {
 	var clickeddiv = document.getElementById("el" + x + "x" + y + "x" + z);
     if (clickeddiv.style.display === "none") {
